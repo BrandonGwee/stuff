98de7f39a389c41126ccb7f2277e723395e8dc56
BrandonGwee <BG06.23@ichat.sp.edu.sg>
2024-02-27 16:48:32 +0800

    added break statements to switch

--- js/me.js
+++ js/me.js
@@ -11,9 +11,9 @@ function greeting() {
     let time_of_day = "";
 
     switch(hours){
-        case (0 < hours || hours < 11): time_of_day = "morning";
-        case (12 < hours || hours < 16): time_of_day = "afternoon";
-        case (16 < hours || hours < 24): time_of_day = "evening";
+        case (0 < hours || hours < 11): time_of_day = "morning"; break;
+        case (12 < hours || hours < 16): time_of_day = "afternoon"; break;
+        case (16 < hours || hours < 24): time_of_day = "evening"; break;
     }
     
     console.log(time_of_day)
