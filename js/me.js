ce90174e9b1e64c57cdd4a15a3d708bac53f515e
BrandonGwee <BG06.23@ichat.sp.edu.sg>
2024-02-27 16:56:47 +0800

    time of day works

--- js/me.js
+++ js/me.js
@@ -4,19 +4,19 @@ document.addEventListener("DOMContentLoaded", (event) => {
 })
 
 function greeting() {
-    let greet_message = ""
+    let greet_message = ``;
     const now = new Date();
     const hours = now.getHours();
 
     let time_of_day = "";
 
-    switch(hours){
-        case (0 < hours || hours < 11): time_of_day = "morning"; break;
-        case (12 < hours || hours < 16): time_of_day = "afternoon"; break;
-        case (16 < hours || hours < 24): time_of_day = "evening"; break;
+    if(0 < hours && hours < 11) {
+        time_of_day = "morning";
+    } else if (11 < hours && hours < 16) {
+        time_of_day = "afternoon";
+    } else if (hours < 24) {
+        time_of_day = "evening";
     }
-    
-    console.log(time_of_day)
 
     greet_message = `Good ${time_of_day}, I'm Brandon.`;
 
