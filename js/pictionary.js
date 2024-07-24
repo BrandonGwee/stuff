e770a907e5bca0de482d79b5f0b53d16de33aaf8
BrandonGwee <BG06.23@ichat.sp.edu.sg>
2024-07-24 11:30:27 +0800

    giving up on concat for strings, used +

--- js/pictionary.js
+++ js/pictionary.js
@@ -14,21 +14,21 @@ function randomNum (range_min, range_max) {
 function gibberish() {
     let output = '';
 
-    output = concat(prefixes[randomNum(0, prefixes.length)], 
-    consonants[randomNum(0, consonants.length)],
-    vowels[randomNum(0, vowels.length)]);
+    output += prefixes[randomNum(0, prefixes.length)]
+    + consonants[randomNum(0, consonants.length)]
+    + vowels[randomNum(0, vowels.length);
 
     for (let i = 0; i < randomNum(0, 6); i++) {
         if (randomNum(0,6) < 3) {
-            output = concat(output, vowels[randomNum(0, vowels.length)]);
+            output += vowels[randomNum(0, vowels.length)];
         } 
         else {
-            output = concat(output, consonants[randomNum(0, consonants.length)]);
+            output += consonants[randomNum(0, consonants.length)];
         }
     }
 
-    concat(output, consonants[randomNum(0, consonants.length)], 
-    (suffixes[randomNum(0, suffixes.length)]));
+    output += consonants[randomNum(0, consonants.length)]
+    + (suffixes[randomNum(0, suffixes.length)]);
 
     return output;
 }
