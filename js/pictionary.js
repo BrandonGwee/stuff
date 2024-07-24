44224223dd5d1be563448b45589920e75f1c65db
BrandonGwee <BG06.23@ichat.sp.edu.sg>
2024-07-24 11:30:43 +0800

    forgot closing sq bracket

--- js/pictionary.js
+++ js/pictionary.js
@@ -16,7 +16,7 @@ function gibberish() {
 
     output += prefixes[randomNum(0, prefixes.length)]
     + consonants[randomNum(0, consonants.length)]
-    + vowels[randomNum(0, vowels.length);
+    + vowels[randomNum(0, vowels.length)];
 
     for (let i = 0; i < randomNum(0, 6); i++) {
         if (randomNum(0,6) < 3) {
