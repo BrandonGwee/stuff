c2eec914fb2b953fa866bd5594e4c084b84efff7
BrandonGwee <BG06.23@ichat.sp.edu.sg>
2024-07-24 10:31:11 +0800

    array concatonating

--- js/gibberish.js
+++ js/gibberish.js
@@ -1,24 +1,23 @@
-export const prefixes = concat(
+export const prefixes = 
     [
         "anti", "a", "de", "dis", "im",
         "un", "over", "extra", "mega", "inter", "in", "counter-", "semi", "sub",
         "under", "mis", "post", "pre", "re", "con", "contra", "ante", "co", "an"
-    ], 
+    ].concat( 
 new Array(20));
 
-export const suffixes = concat(
+export const suffixes = 
     [
         "acy", "al", "ance", "dom", "er", "or", "ism", "ist", "ity",
         "ment", "ness", "ion", "able", "ible", "ful", "ess", "ish", "ive", "ious", "ous", "y",
         "ian", "ia", "ium", "nant", "s", "es"
-    ],
- new Array(20));
+    ]
+ .concat(new Array(20));
 
-export const weighted_consonants = concat(
-    ['j', 'r', 'v', 'x', 'z'],
-    Array(2).fill(['b', 'c', 'd', 'f', 'g', 'h', 'k', 'l', 'm', 'n', 'p', 'w']).flat(),
-    Array(3).fill(['r', 's', 't']).flat()
-);
+export const weighted_consonants =
+    ['j', 'r', 'v', 'x', 'z']
+    .concat(Array(2).fill(['b', 'c', 'd', 'f', 'g', 'h', 'k', 'l', 'm', 'n', 'p', 'w']).flat())
+    .concat(Array(3).fill(['r', 's', 't']).flat());
 
 export const weighted_vowels = "aeiouyaei".split("");
 
